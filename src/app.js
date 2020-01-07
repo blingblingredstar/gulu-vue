571c36b1d9d112cbd97eaf332f55b8b00e5f04db
liuyong <564447023@qq.com>
2020-01-07 11:20:04 +0800

    使用chai添加四个测试用例

--- src/app.js
+++ src/app.js
@@ -16,3 +16,74 @@ const app = new Vue({
     loading3: false
   }
 });
+
+// Unit test
+import { expect } from "chai";
+
+const testElement = document.getElementById("test");
+
+{
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: "settings"
+    }
+  });
+  vm.$mount();
+  let useElement = vm.$el.querySelector("use");
+  let href = useElement.getAttribute("xlink:href");
+  expect(href).to.eq("#i-" + "settings");
+  vm.$el.remove();
+  vm.$destroy();
+}
+
+{
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: "settings",
+      loading: true
+    }
+  });
+  vm.$mount();
+  let useElement = vm.$el.querySelector("use");
+  let href = useElement.getAttribute("xlink:href");
+  expect(href).to.eq("#i-" + "loading");
+  vm.$el.remove();
+  vm.$destroy();
+}
+
+{
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: "settings"
+    }
+  });
+  vm.$mount(div);
+  let svgElement = vm.$el.querySelector("svg");
+  let { order } = window.getComputedStyle(svgElement);
+  expect(order).to.eq("1");
+  vm.$el.remove();
+  vm.$destroy();
+}
+
+{
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: "settings",
+      iconPosition: "right"
+    }
+  });
+  vm.$mount(div);
+  let svgElement = vm.$el.querySelector("svg");
+  let { order } = window.getComputedStyle(svgElement);
+  expect(order).to.eq("2");
+  vm.$el.remove();
+  vm.$destroy();
+}
