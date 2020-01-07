2b2a512bfa913c62d01f662425249eb3ceb1afe0
liuyong <564447023@qq.com>
2020-01-07 10:12:49 +0800

    创建button-group组件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 
 import Button from "./button.vue";
+import ButtonGroup from "./button-group.vue";
 import Icon from "./icon.vue";
 
 Vue.component("g-button", Button);
+Vue.component("g-button-group", ButtonGroup);
 Vue.component("g-icon", Icon);
 
 const app = new Vue({
