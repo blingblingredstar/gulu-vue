73d6bfc27155b1865a7457dc08f17352a13c9e5c
liuyong <564447023@qq.com>
2020-01-08 15:34:15 +0800

    初步完成row和col组件

--- src/app.js
+++ src/app.js
@@ -4,11 +4,15 @@ import Button from './button.vue'
 import ButtonGroup from './button-group.vue'
 import Icon from './icon.vue'
 import Input from './input.vue'
+import Row from './row.vue'
+import Col from './col.vue'
 
 Vue.component('g-button', Button)
 Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-icon', Icon)
 Vue.component('g-input', Input)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
 
 const app = new Vue({
   el: '#app',
---
+++ src/col.vue
@@ -0,0 +1,38 @@
+<template>
+  <div :class="['col', {['col-'+ span]: span}]">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GuluCol',
+  props: {
+    span: {
+      type: [Number, String],
+      validator(value) {
+        if (typeof value === 'string') {
+          value = +value
+        }
+        return value > 0 && value <= 24
+      },
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.col {
+  height: 100px;
+  background: red;
+  flex-basis: 50%;
+  border: 1px solid blue;
+
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      flex-basis: ($n / 24) * 100%;
+    }
+  }
+}
+</style>
