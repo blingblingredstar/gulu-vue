ab624ebdcf6bcaa5d4d737bebd13751aaedf8e68
liuyong <564447023@qq.com>
2020-01-09 12:41:21 +0800

    初步实现响应式布局

--- src/col.vue
+++ src/col.vue
@@ -5,6 +5,20 @@
 </template>
 
 <script>
+const validator = (value) => {
+  const keys = Object.keys(value)
+  const props = ['span', 'offset']
+  let isValid = true
+
+  keys.forEach((key) => {
+    if (!props.includes(key)) {
+      isValid = false
+    }
+  })
+
+  return isValid
+}
+
 export default {
   name: 'GuluCol',
   data() {
@@ -31,11 +45,26 @@ export default {
         return value > 0 && value <= 24
       },
     },
+    phone: { type: Object, validator },
+    pad: { type: Object, validator },
+    narrowPc: { type: Object, validator },
+    pc: { type: Object, validator },
+    widePc: { type: Object, validator },
   },
   computed: {
     colClass() {
-      const { span, offset } = this
-      return ['col', span && `col-${span}`, offset && `offset-${offset}`]
+      const { span, offset, phone, pad, narrowPc, pc, widePc } = this
+
+      return [
+        'col',
+        span && `col-${span}`,
+        offset && `offset-${offset}`,
+        phone && phone.span && `col-phone-${phone.span}`,
+        pad && pad.span && `col-pad-${pad.span}`,
+        narrowPc && narrowPc.span && `col-narrow-pc-${narrowPc.span}`,
+        pc && pc.span && `col-pc-${pc.span}`,
+        widePc && widePc.span && `col-wide-pc-${widePc.span}`,
+      ]
     },
     colStyle() {
       return {
@@ -50,7 +79,6 @@ export default {
 <style lang="scss" scoped>
 .col {
   height: 100px;
-  flex-basis: 50%;
 
   $class-prefix: col-;
   @for $n from 1 through 24 {
@@ -63,6 +91,86 @@ export default {
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       margin-right: ($n / 24) * 100%;
+    }
+  }
+  /* phone */
+  @media (max-width: 576px) {
+    $class-prefix: col-phone-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        flex-basis: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-phone-;
+    @for $n from 0 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-right: ($n / 24) * 100%;
+      }
+    }
+  }
+  /* pad */
+  @media (min-width: 577px) and (max-width: 768px) {
+    $class-prefix: col-pad-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        flex-basis: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-pad-;
+    @for $n from 0 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-right: ($n / 24) * 100%;
+      }
+    }
+  }
+  /* narrow-pc */
+  @media (min-width: 769px) and (max-width: 992px) {
+    $class-prefix: col-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        flex-basis: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-narrow-pc-;
+    @for $n from 0 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-right: ($n / 24) * 100%;
+      }
+    }
+  }
+  /* pc */
+  @media (min-width: 993px) and (max-width: 1200px) {
+    $class-prefix: col-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        flex-basis: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-pc-;
+    @for $n from 0 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-right: ($n / 24) * 100%;
+      }
+    }
+  }
+  /* wide-pc */
+  @media (min-width: 1201px) {
+    $class-prefix: col-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        flex-basis: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-wide-pc-;
+    @for $n from 0 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-right: ($n / 24) * 100%;
+      }
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -41,6 +41,8 @@ export default {
 <style lang="scss" scoped>
 .row {
   display: flex;
+  flex-wrap: wrap;
+
   &.align-left {
     justify-content: flex-start;
   }
